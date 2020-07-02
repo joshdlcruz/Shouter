2c85592980ac1a18d9d4b92de6c867f82b0f52c0
joshdlcruz <dcjoshua25@gmail.com>
2020-07-02 10:26:29 +0800

    signup form & auth state

--- src/pages/login.js
+++ src/pages/login.js
@@ -12,33 +12,9 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-const styles = {
-  form: {
-    textAlign: 'center',
-  },
-  image: {
-    margin: '20px auto 20px auto',
-  },
-  pageTitle: {
-    margin: '10px auto 10px auto',
-  },
-  textField: {
-    margin: '10px auto 10px auto',
-  },
-  button: {
-    marginTop: 20,
-    marginBottom: 20,
-    position: 'relative',
-  },
-  customError: {
-    color: 'red',
-    fontSize: '0.8rem',
-    marginTop: 10,
-  },
-  progress: {
-    position: 'absolute',
-  },
-};
+const styles = (theme) => ({
+  ...theme.spreadThis,
+});
 
 export class login extends Component {
   constructor() {
@@ -63,6 +39,7 @@ export class login extends Component {
       .post('/login', userData)
       .then((res) => {
         console.log(res.data);
+        localStorage.setItem('FBIdToken', `Bearer ${res.data.token}`);
         this.setState({
           loading: false,
         });
